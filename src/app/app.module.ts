e5111987d0be92620b60954332478342e4374f3a
Bea <bcampdat@gmail.com>
2024-06-24 09:41:41 +0200

    commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioModule } from './inicio/inicio.module';
+
 
 @NgModule({
   declarations: [
@@ -10,7 +12,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    InicioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
