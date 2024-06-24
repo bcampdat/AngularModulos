1a5441a8eda9c8c75b6aaf39702d937ab879a339
Bea <bcampdat@gmail.com>
2024-06-24 11:34:38 +0200

    update route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './inicio/home/home.component';
 import { NotFoundComponent } from './inicio/not-found/not-found.component';
+import { ConsultaRecetasComponent } from './recetas/consulta-recetas/consulta-recetas.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'recetas', component: ConsultaRecetasComponent },
   { path: '**', component: NotFoundComponent }
 
 ];
--- src/app/recetas/recetas.service.ts
+++ src/app/recetas/recetas.service.ts
@@ -1,9 +1,19 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ICategoriasMeal, IMeals } from './meal.interface';
 
 @Injectable({
-  providedIn: 'root'  
+  providedIn: 'root'
 })
 export class RecetasService {
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  getRecetas(categoria: string): Observable<IMeals> {
+    return this.http.get<IMeals>(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${categoria}`);
+  }
+
+  getCategorias(): Observable<ICategoriasMeal> {
+    return this.http.get<ICategoriasMeal>(`https://www.themealdb.com/api/json/v1/1/list.php?c=list`);
+  }
 }
