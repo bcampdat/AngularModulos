c7c72d7d5af0a9de887cc1fe4ade0502580e3a7d
Bea <bcampdat@gmail.com>
2024-06-24 13:45:20 +0200

    pie

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,18 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
 import { RouterModule } from '@angular/router';
+import { RelojComponent } from './reloj/reloj.component';
+import { PieComponent } from './pie/pie.component';
 
 
 @NgModule({
   declarations: [
-    NavbarComponent
+    NavbarComponent,
+    RelojComponent,
+    PieComponent
   ],
   imports: [
     CommonModule,
     RouterModule
   ],
   exports: [
-    NavbarComponent
+    NavbarComponent,
+    RelojComponent,
+    PieComponent
   ]
 })
 export class SharedModule { }
